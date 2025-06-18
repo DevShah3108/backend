ef5b141c562903d6b6d9e720207b5866ac15e44c
Dev Shah <devshah31804@gmail.com>
2025-06-18 11:51:56 +0530

    Updated backend code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Configure CORS dynamically
 const allowedOrigins = process.env.FRONTEND_URL 
